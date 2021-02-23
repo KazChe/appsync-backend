1afdab672767abd6f0cfa189aab1576d3397c9e4
KazChe <kaz.che@gmail.com>
2021-02-23 12:24:31 -0800

    added given.a_random_user step

--- __tests__/test_cases/integration/comfirm-user-signup.js
+++ __tests__/test_cases/integration/comfirm-user-signup.js
@@ -1,6 +1,6 @@
-const given = require()
-const when = require()
-const then = require()
+const given = require('../../steps/given')
+const when = require('../../steps/when')
+const then = require('../../steps/then')
 const chance = require('chance').Chance()
 
 describe("when a confirmUserSignup runs", () => {
