5c48666a23d6a3fb0a84f0b44764c89a118b7f85
KazChe <kaz.che@gmail.com>
2021-02-23 11:55:53 -0800

    skeleton of confirm-user-sign-up integration test

--- __tests__/test_cases/integration/comfirm-user-signup.js
+++ __tests__/test_cases/integration/comfirm-user-signup.js
@@ -1,3 +1,18 @@
+const given = require()
+const when = require()
+const then = require()
+const chance = require('chance').Chance()
 
+describe("when a confirmUserSignup runs", () => {
+    it("should save user profile in DynamoDB", async () => {
+        const { name, email } = given.a_random_user()
+        const username = chance.guid()
 
-describe()
+        await when.we_invoke_confirmUserSignup(username, name, email)
+
+        const ddbUser = await then.user_exists_in_UsersTable(username)
+        expect(ddbUser).toEqual({
+
+        })
+    })
+})
