9dae56a06c8836ace5019a0efee722b497d74f5e
KazChe <kaz.che@gmail.com>
2021-03-12 13:50:29 -0800

    add skeleton for getMyProfile unit test

---
+++ __tests__/test_cases/unit/Query.getMyProfile.request.js
@@ -0,0 +1,8 @@
+const given = require('../../steps/given')
+const when = require('../../steps/then')
+
+describe('Query.getMyProfile.request template', () => {
+    it("Should use username as 'id'", () => {
+
+    })
+})
