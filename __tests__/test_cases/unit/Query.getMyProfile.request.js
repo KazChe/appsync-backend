8455ade37ee891df5db5c86c72ba2a342cb4d5e6
KazChe <kaz.che@gmail.com>
2021-03-18 13:03:11 -0700

    add skeleton for getMyProfile unit test

--- __tests__/test_cases/unit/Query.getMyProfile.request.js
+++ __tests__/test_cases/unit/Query.getMyProfile.request.js
@@ -4,6 +4,7 @@ const chance = require('chance').Chance()
 
 describe('Query.getMyProfile.request template', () => {
     it("Should use username as 'id'", () => {
-
+        const username = chance.guid()
+        const result = when.we_invoke_an_appsync_template()
     })
 })
