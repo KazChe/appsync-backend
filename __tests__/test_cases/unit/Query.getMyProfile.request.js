3974c4a8964cf544c27f9392c1bde3300f93991d
KazChe <kaz.che@gmail.com>
2021-03-22 13:49:21 -0700

    add skeleton for getMyProfile unit test

--- __tests__/test_cases/unit/Query.getMyProfile.request.js
+++ __tests__/test_cases/unit/Query.getMyProfile.request.js
@@ -7,7 +7,7 @@ describe('Query.getMyProfile.request template', () => {
     it("Should use username as 'id'", () => {
         const templatePath = path.resolve(__dirname, '../../../mapping-templates/Query.getMyProfile.request.vtl')
         const username = chance.guid()
-        const result = when.we_invoke_an_appsync_template()
+        const result = when.we_invoke_an_appsync_template(templatePath, context)
 
         expect(result).toEqual({
             "version" : "2018-05-29",
